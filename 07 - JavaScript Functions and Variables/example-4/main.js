7a0acdea8ee9e6c4d87ef11ae17854d4961c1fe6
Severiano Badajoz <sevebadajoz@gmail.com>
2018-09-20 11:53:37 -0700

    Complete first challenge

--- 07 - JavaScript Functions and Variables/example-4/main.js
+++ 07 - JavaScript Functions and Variables/example-4/main.js
@@ -1,2 +1,18 @@
 // Your scripts here
 
+
+// define a function
+// get the value of bill and assign to const
+
+const elem = document.querySelector('#calculate');
+
+elem.onclick = (e) => {
+    const bill = document.querySelector('#amount').value;
+    const tipPercent = document.querySelector('#percent').value;
+
+    const tip = bill * tipPercent / 100;
+    const total = Number(bill) + tip;
+
+    document.querySelector('#tip').innerHTML = tip;
+    document.querySelector('#total').innerHTML = bill + tip;
+}
