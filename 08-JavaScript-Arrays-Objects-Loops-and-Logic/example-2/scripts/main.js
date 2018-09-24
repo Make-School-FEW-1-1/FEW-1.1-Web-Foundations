ce82897bd74141c3185559f99238ac31803a6b73
Severiano Badajoz <sevebadajoz@gmail.com>
2018-09-24 15:12:02 -0700

    Complete challenge 2

--- 08-JavaScript-Arrays-Objects-Loops-and-Logic/example-2/scripts/main.js
+++ 08-JavaScript-Arrays-Objects-Loops-and-Logic/example-2/scripts/main.js
@@ -1,6 +1,16 @@
 // Loop through the contents of data
 
-for (let i = 0; i < data.length; i += 1) {
-  console.log(i, data[i].first_name);
+let str = '';
+for (let i = 0; i < data.length; ++i) {
+    str += htmlTemp(data[i]);
 }
+document.querySelector('#content').innerHTML = str;
 
+function htmlTemp(data) {
+    const date = new Date(data.date).toDateString();
+    const { prefix, first_name, last_name, image} = data;
+    return `<div class="user" style="display:flex; justify-content: space-between; border: 1px solid #ddd; margin: 1em; padding: .5em;">
+                <h3>${prefix} ${first_name} ${last_name}</h3>
+                <p> ${date} <img src="${image}"></p>
+                </div>`;
+}
